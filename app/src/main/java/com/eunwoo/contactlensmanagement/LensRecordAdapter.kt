1d7506bc12942c9cc90f11f4bcb65c0f3097defa
Oscar <dn0963@naver.com>
2021-10-31 23:27:08 +0900

    Add TestDialog to lens_management_fragment

---
+++ app/src/main/java/com/eunwoo/contactlensmanagement/LensRecordAdapter.kt
@@ -0,0 +1,55 @@
+package com.eunwoo.contactlensmanagement
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.eunwoo.contactlensmanagement.databinding.LensRecordItemBinding
+
+class LensRecordAdapter(val db: LensDatabase, var items: List<Lens>?)
+    : RecyclerView.Adapter<LensRecordAdapter.ViewHolder>() {
+
+    lateinit var binding: LensRecordItemBinding
+    lateinit var mContext: Context
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
+            : ViewHolder {
+        binding = LensRecordItemBinding
+                .inflate(LayoutInflater.from(parent.context), parent, false)
+        mContext = parent.context
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items!!.get(position),position)
+    }
+
+    override fun getItemCount(): Int {
+        return items!!.size
+    }
+    fun getItem(): List<Lens>?{
+        return items
+    }
+
+
+    inner class ViewHolder(private val binding: LensRecordItemBinding): RecyclerView.ViewHolder(binding.root){
+        var index: Int? = null
+        fun bind(lens: Lens, position: Int) {
+            index = position
+            binding.textView.setText(lens.id.toString())
+            binding.textView2.setText(lens.name)
+            binding.textView3.setText(lens.contents)
+            binding.button.setOnClickListener { editData(binding.textView.text.toString()) }
+        }
+        fun editData(contents: String){
+            Thread {
+                index?.let { items!!.get(it).contents = contents };
+                index?.let { items!!.get(it) }?.let { db.lensDao().update(it) };
+            }.start()
+            Toast.makeText(mContext,"저장완료", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
--- app/src/main/java/com/eunwoo/contactlensmanagement/fragment/LensManagementFragment.kt
+++ app/src/main/java/com/eunwoo/contactlensmanagement/fragment/LensManagementFragment.kt
@@ -6,8 +6,15 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.eunwoo.contactlensmanagement.Lens
+import com.eunwoo.contactlensmanagement.LensDatabase
+import com.eunwoo.contactlensmanagement.LensRecordAdapter
 import com.eunwoo.contactlensmanagement.R
 import com.eunwoo.contactlensmanagement.databinding.LensManagementFragmentBinding
 
@@ -22,6 +29,11 @@ class LensManagementFragment: Fragment() {
     }
 
     private lateinit var binding: LensManagementFragmentBinding
+    private lateinit var adapter: LensRecordAdapter
+
+    private lateinit var db: LensDatabase
+
+    private lateinit var _context: Context
 
     // 메모리 올라갔을 때
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +45,7 @@ class LensManagementFragment: Fragment() {
     override fun onAttach(context: Context) {
         super.onAttach(context)
         Log.d(TAG, "LensManagementFragment - onAttach() called")
+        _context = context
     }
 
     // 뷰가 생성되었을 때
@@ -47,8 +60,44 @@ class LensManagementFragment: Fragment() {
 
         binding = LensManagementFragmentBinding.inflate(inflater, container, false)
 
+//        initSwipe()
 
+        binding.lensAddButton.setOnClickListener {
+            // title 입력 다이얼로그를 호출한다.
+            // title 입력하여 리사이클러뷰 addItem
+            val edittext = EditText(_context)
+            val builder: AlertDialog.Builder = AlertDialog.Builder(_context)
+            builder.setTitle("Item 추가")
+            builder.setMessage("제목을 입력해 주세요.")
+            builder.setView(edittext)
+            builder.setPositiveButton("입력"
+            ) { dialog, which ->
+                //제목 입력, DB추가
+                if (!edittext.text.toString().isEmpty()) {
+                    Thread(Runnable {
+                        db!!.lensDao().insert( Lens(null, edittext.text.toString(), null))
+                    }).start()
+                }
+            }
+            builder.setNegativeButton("취소"
+            ) { dialog, which ->
+                //취소
+            }
+            builder.show()
+        }
+        db = LensDatabase.getInstance(_context)
+        binding.lensRecordRecyclerview.setHasFixedSize(true)
+        val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(_context)
+        binding.lensRecordRecyclerview.layoutManager = layoutManager
+
+        //UI 갱신 (라이브데이터 Observer 이용, 해당 디비값이 변화가생기면 실행됨)
+        db!!.lensDao().getAll().observe(viewLifecycleOwner, androidx.lifecycle.Observer{
+            // update UI
+            adapter = LensRecordAdapter(db!!,it)
+            binding.lensRecordRecyclerview.adapter = adapter
+        })
 
         return binding.root
     }
+
 }

18b645768f3a62328af3fd100ee9b35a8e2b765d
Oscar0819 <dn08193@gmail.com>
2021-12-29 23:29:51 +0900

    Add permission check

--- app/src/main/java/com/eunwoo/contactlensmanagement/MainActivity.kt
+++ app/src/main/java/com/eunwoo/contactlensmanagement/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Base64
 import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.eunwoo.contactlensmanagement.databinding.ActivityMainBinding
 import com.eunwoo.contactlensmanagement.fragment.LensManagementFragment
 import com.eunwoo.contactlensmanagement.fragment.MapFragment
@@ -23,6 +25,7 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         const val TAG: String = "로그"
+        val MY_PERMISSION_ACCESS_ALL = 100
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,8 +37,10 @@ class MainActivity : AppCompatActivity() {
 
         Log.d(TAG, "MainActivity - onCreate() called")
         initBottomNavigationBar(binding)
+        checkPermission()
     }
 
+    // 바텀네비바 초기
     private fun initBottomNavigationBar(binding: ActivityMainBinding) {
         binding.bnvMain.setOnItemSelectedListener {
             when (it.itemId) {
@@ -62,4 +67,32 @@ class MainActivity : AppCompatActivity() {
         binding.bnvMain.selectedItemId = R.id.lens_management_fragment_item
     }
 
+    // 권한 확
+    private fun checkPermission() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            var permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
+            ActivityCompat.requestPermissions(this, permissions, MY_PERMISSION_ACCESS_ALL)
+        }
+    }
+
+    // 권한 요청 결과 반환
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            MY_PERMISSION_ACCESS_ALL -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this@MainActivity, "위치권한이 허용되었습니다.", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this@MainActivity, "위치권한을 거절하셨습니다. 관련 서비스를 제공 받으시려면 설정에 가셔서 위치권한을 허용해주세요.", Toast.LENGTH_LONG).show()
+                }
+                return
+            }
+        }
+    }
 }
